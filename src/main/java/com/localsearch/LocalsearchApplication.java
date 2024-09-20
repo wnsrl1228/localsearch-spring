e57700bd6f9e4a9a6065ecafc192c81a83af4181
wnsrl <wnsrl981228@gmail.com>
2024-09-20 15:59:56 +0900

    first commit

---
+++ src/main/java/com/localsearch/LocalsearchApplication.java
@@ -0,0 +1,13 @@
+package com.localsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalsearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/localsearch/LocalsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
