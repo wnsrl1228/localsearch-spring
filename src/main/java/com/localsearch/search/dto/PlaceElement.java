97e0181cfe38e14c415131b330ea89cc52136522
wnsrl <wnsrl981228@gmail.com>
2024-09-22 20:42:18 +0900

    bug: Place에 null 처리

--- src/main/java/com/localsearch/search/dto/PlaceElement.java
+++ src/main/java/com/localsearch/search/dto/PlaceElement.java
@@ -3,6 +3,8 @@ package com.localsearch.search.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Optional;
+
 @Getter
 @Builder
 public class PlaceElement {
@@ -18,9 +20,11 @@ public class PlaceElement {
         return PlaceElement.builder()
                 .id(apiPlace.getId())
                 .displayName(apiPlace.getDisplayName().getText())
-                .primaryTypeDisplayName(apiPlace.getPrimaryTypeDisplayName().getText())
-                .formattedAddress(apiPlace.getFormattedAddress())
-                .googleMapsUri(apiPlace.getGoogleMapsUri())
+                .primaryTypeDisplayName(Optional.ofNullable(apiPlace.getPrimaryTypeDisplayName())
+                        .map(LocalSearchAPIResponse.PrimaryTypeDisplayName::getText)
+                        .orElse(""))
+                .formattedAddress(Optional.ofNullable(apiPlace.getFormattedAddress()).orElse(""))
+                .googleMapsUri(Optional.ofNullable(apiPlace.getGoogleMapsUri()).orElse(""))
                 .latitude(apiPlace.getLocation().getLatitude())
                 .longitude(apiPlace.getLocation().getLongitude())
                 .build();
